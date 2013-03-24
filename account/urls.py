08fbefae15ba9a2f1adbdd2cdc8a4f0b7a18a734
root <root@6e920c7a-57e7-4280-be05-859c329806b9.cs27b8cloud.internal>
2013-03-24 20:53:45 +0900

    join

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-
+						url(r'^join/$', 'account.views.join_view'),
+						url(r'^join_ok/$', 'account.views.join_ok'),
 )
--- account/views.py
+++ account/views.py
@@ -1,1 +1,28 @@
-# Create your views here.
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render
+from django import forms
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+
+def join_view(request):
+
+	if request.user.is_authenticated():
+		return HttpResponseRedirect('/')
+	else:
+		return render(request, 'join.djhtml')
+
+@csrf_exempt
+def join_ok(request):
+
+	if request.method == "POST":
+		try:
+			id = request.POST['id']
+			password = request.POST['password']
+			email = request.POST['email']
+
+		except:
+			pass
+
+	else:
+		return HttpResponseRedirect('/')
